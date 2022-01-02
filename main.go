9aa43ae6a0a41d4ed627ba5cff4b7a8562f21677
Linden <70739041+linden@users.noreply.github.com>
2022-01-02 09:17:42 -0800

    feat: added session `ID` & stylized go errors

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ var (
 	yellow = "\u001b[33m"
 	green  = "\u001b[32m"
 	purple = "\u001b[35m"
-	clear  = "\u001b[0m"
+	black  = "\u001b[30m"
+
+	whiteBG = "\u001b[47m"
+
+	clear = "\u001b[0m"
+	bold  = "\u001b[1m"
 )
 
 func main() {
@@ -44,11 +49,13 @@ func main() {
 func pipe(title string, a net.Conn, b net.Conn) {
 	buffer := make([]byte, 65535)
 
+	ID := "undefined"
+
 	for {
 		length, err := a.Read(buffer)
 
 		if err != nil {
-			fmt.Println(err)
+			issue(err)
 			return
 		}
 
@@ -61,6 +68,10 @@ func pipe(title string, a net.Conn, b net.Conn) {
 
 			if len(split) == 2 {
 				formatted += fmt.Sprintf("NEWLINETAB%sTAB%s%s%s", split[0], red, split[1], clear)
+
+				if split[0] == "ID" {
+					ID = split[1]
+				}
 			} else {
 				if index != 0 {
 					formatted += " "
@@ -77,13 +88,17 @@ func pipe(title string, a net.Conn, b net.Conn) {
 		formatted = strings.ReplaceAll(formatted, "NEWLINE", "\n")
 		formatted = strings.ReplaceAll(formatted, "TAB", "\t")
 
-		fmt.Printf("%s: %s\n", title, formatted)
+		fmt.Printf("%s%s%s %s %s\n", bold, ID, clear, title, formatted)
 
 		_, err = b.Write(cursor)
 
 		if err != nil {
-			fmt.Println(err)
+			issue(err)
 			return
 		}
 	}
+}
+
+func issue(err error) {
+	fmt.Printf("%s%sunexpected issue: %v%s\n", black, whiteBG, err, clear)
 }
